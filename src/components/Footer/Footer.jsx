2f7092ea6568d281e0848d6ed87d16a9d1037db3
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-04-23 14:35:02 +0100

    update:footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-
 import { Link } from "react-scroll/modules";
 // import { Link } from "react-router-dom";
 import { FaDiscord, FaTwitter } from "react-icons/fa";
@@ -13,113 +12,73 @@ function Footer() {
       >
         <div className="container py-5">
           <div className="row g-5">
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-6 col-md-6">
               <h4
                 className="section-title ff-secondary text-start
                         text-warning fw-normal mb-4"
               >
                 Kadena Komodos
               </h4>
-              <Link
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-                className="btn btn-link"
-              >
-                About Us
-              </Link>
-              <Link
-                to="road"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-                className="btn btn-link"
-              >
-                Roadmap
-              </Link>
-              <Link
-                to="team"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-                className="btn btn-link"
-              >
-                Team
-              </Link>
+              <div style={{ display: "flex", gap: "10px" }}>
+                <Link
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  className="btn btn-link"
+                >
+                  About Us
+                </Link>
+                <Link
+                  to="road"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  className="btn btn-link"
+                >
+                  Roadmap
+                </Link>
+                <Link
+                  to="team"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  className="btn btn-link"
+                >
+                  Team
+                </Link>
+              </div>
             </div>
-            <div className="col-lg-3 col-md-6" id="contact">
+            <div className="col-lg-6 col-md-6" id="contact">
               <h4
                 className="section-title ff-secondary text-start text-warning
                         fw-normal mb-4"
               >
                 Contact
               </h4>
-              <p className="mb-2 ">
-                <a href="https://discord.gg/JxE2KfJxtU" className="text-light">
-                  {/* <FontAwesomeIcon icon={faMaLinkMarked} className="me-3" /> */}
-                  <FaDiscord className="me-3 text-light" />
-                  Discord
-                </a>
-              </p>
-              <p className="mb-2 ">
-                <a
-                  href="https://twitter.com/KadenaKomodos"
-                  className="text-light"
-                >
-                  {/* <FontAwesomeIcon icon={faMaLinkMarked} className="me-3" /> */}
-                  <FaTwitter className="me-3 text-light" />
-                  Twitter
-                </a>
-              </p>
 
-              <div className="d-flex pt-2">
-                {/* <a href="" className="btn btn-outline-light btn-social">
-                                <FontAwesomeIcon icon={faTwitter} />
-                            </a> */}
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="text-light">
-                <h4
-                  className="section-title ff-secondary text-start text-warning
-                                fw-normal mb-4"
-                >
-                  Opening
-                </h4>
-                <h5 className="text-light fw-normal">Monday - Saturday</h5>
-                <p>24/7</p>
-                <h5 className="text-light fw-normal">Sunday</h5>
-                <p>24/7</p>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="text-light">
-                <h4
-                  className="section-title ff-secondary text-start text-warning
-                                fw-normal mb-4"
-                >
-                  Newsletter
-                </h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <div className="position-relative mx-auto" max-width="400px">
-                  <input
-                    className="form-control border-warning w-100 py-3
-                                ps-4 pe-5"
-                    type="text"
-                    placeholder="Your Email"
-                  />
-                  <button
-                    type="button"
-                    className="btn btn-warning py-2 position-absolute
-                                top-0 end-0 mt-2 me-2"
+              <div style={{ display: "flex", gap: "10px" }}>
+                <p className="mb-2 ">
+                  <a
+                    href="https://discord.gg/JxE2KfJxtU"
+                    className="text-light"
                   >
-                    Subscribe
-                  </button>
-                </div>
+                    <FaDiscord className="me-3 text-light" />
+                    Discord
+                  </a>
+                </p>
+                <p className="mb-2 ">
+                  <a
+                    href="https://twitter.com/KadenaKomodos"
+                    className="text-light"
+                  >
+                    <FaTwitter className="me-3 text-light" />
+                    Twitter
+                  </a>
+                </p>
               </div>
             </div>
           </div>
