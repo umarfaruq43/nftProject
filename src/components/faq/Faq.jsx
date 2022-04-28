6320636f978d38e6fd91fafc88085546b17a1e66
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-04-28 17:56:32 +0100

    update

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -8,14 +8,17 @@ const faq = () => {
           FAQ
         </h4>
         <div className="accordion" id="accordionExample">
+          
+          {/* ************************ */}
+
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingOne">
               <button
-                className="accordion-button"
+                className="accordion-button collapsed "
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#collapseOne"
-                aria-expanded="true"
+                aria-expanded="false"
                 aria-controls="collapseOne"
               >
                 When is the Mint going to be?
@@ -23,7 +26,7 @@ const faq = () => {
             </h2>
             <div
               id="collapseOne"
-              className="accordion-collapse collapse show"
+              className="accordion-collapse collapse"
               aria-labelledby="headingOne"
               data-bs-parent="#accordionExample"
             >
@@ -34,6 +37,8 @@ const faq = () => {
               </div>
             </div>
           </div>
+
+          {/* *********************************** */}
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingTwo">
               <button
@@ -90,22 +95,22 @@ const faq = () => {
           </div>
 
           <div className="accordion-item">
-            <h2 className="accordion-header" id="headingThree">
+            <h2 className="accordion-header" id="headingFour">
               <button
                 className="accordion-button collapsed "
                 type="button"
                 data-bs-toggle="collapse"
-                data-bs-target="#collapseThree"
+                data-bs-target="#collapseFour"
                 aria-expanded="false"
-                aria-controls="collapseThree"
+                aria-controls="collapseFour"
               >
                 Is there a whitelist?
               </button>
             </h2>
             <div
-              id="collapseThree"
+              id="collapseFour"
               className="accordion-collapse collapse"
-              aria-labelledby="headingThree"
+              aria-labelledby="headingFour"
               data-bs-parent="#accordionExample"
             >
               <div className="accordion-body">
