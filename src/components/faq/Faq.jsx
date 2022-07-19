25407fbdef12f9caa807d2966f8081253bd8d959
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-07-19 18:21:04 +0100

    update: Font fixed, Service components removed

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -8,7 +8,6 @@ const faq = () => {
           FAQ
         </h4>
         <div className="accordion" id="accordionExample">
-          
           {/* ************************ */}
 
           <div className="accordion-item">
@@ -30,7 +29,7 @@ const faq = () => {
               aria-labelledby="headingOne"
               data-bs-parent="#accordionExample"
             >
-              <div className="accordion-body">
+              <div className="accordion-body play">
                 As soon as there is a working, infallible minting system in
                 Kadena. We speculate such a system will be in place sometime in
                 the summer.
@@ -58,7 +57,7 @@ const faq = () => {
               aria-labelledby="headingTwo"
               data-bs-parent="#accordionExample"
             >
-              <div className="accordion-body">
+              <div className="accordion-body play">
                 We are currently waiting on the completion of Marmalade – the
                 main NFT infrastructure being built on Kadena. We may opt for
                 other alternatives should the completion of Marmalade take too
@@ -85,7 +84,7 @@ const faq = () => {
               aria-labelledby="headingThree"
               data-bs-parent="#accordionExample"
             >
-              <div className="accordion-body">
+              <div className="accordion-body play">
                 We will follow the Kadena NFT ecosystem closely: its growth, the
                 minting price of other similar projects, and also the crypto
                 markets as a whole. Hence, we will decide on the price closer to
@@ -113,7 +112,7 @@ const faq = () => {
               aria-labelledby="headingFour"
               data-bs-parent="#accordionExample"
             >
-              <div className="accordion-body">
+              <div className="accordion-body play">
                 Yes, there will be around 200 whitelist spots available. They
                 can be obtained through winning various contests and by being an
                 active member of the community.
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,18 +1,16 @@
 import AboutHome from "../components/Dashboard/AboutHome";
 import Hero from "../components/Hero";
 import ProductsLanding from "../components/Products/ProductsLanding";
-import Services from "../components/Services/Services";
 import Faq from "../components/faq/Faq";
-
 
 function Homepage() {
   return (
     <div>
       <Hero />
-      <Services />
+      {/* <Services /> */}
       <AboutHome />
       <ProductsLanding />
-      
+
       <Faq />
     </div>
   );
